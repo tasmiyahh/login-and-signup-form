472b99615fa6f454a7f277cb206cbc5ebf8d22c4
tasmiyahh <tasmiyahhussainn99@gmail.com>
2022-08-16 00:20:32 +0500

    new change

--- server.mjs
+++ server.mjs
@@ -1,15 +1,105 @@
 import express, { json } from 'express'
-import cors from 'cors'
-const app = express()
+import cors from 'cors';
+import { nanoid } from 'nanoid';
+
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+console.log(nanoid());
+
+let userBase = [];
+
+//to create user
+app.post('/signup', (req, res) => {
+    let body = req.body;
+
+    if (!body.firstName || !body.lastName || !body.email || !body.password) {
+        res.status(400).send(`require field missing.please fill the require field eg:
+    {    
+        
+        "firstName" : "john" , 
+        "lastName" : "doe" ,
+        "email": "abc@gmail.com",
+        "password" : "12345"
+
+    }`);
+
+        return;
+    }
+
+    let newUser = {
+        "id": nanoid(),
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "email": req.body.email,
+        "password": req.body.password
+    }
+
+    userBase.push(newUser);
+    res.status(201).send("user is created");
+
+});
+
+// post because login data is private or get ki body me openly jata h isliye post use
+app.post('/login', (req, res) => {
+    let body = req.body;
+
+    if (!body.email || !body.password) {
+        res.status(400).send(`require field missing.please fill the require field eg:
+    {    
+        
+        "firstName" : "john" , 
+        "lastName" : "doe" ,
+        "email": "abc@gmail.com",
+        "password" : "12345"
+
+    }`);
+
+        return;
+    }
+
+    isfound = false ;
+
+    for (let i = 0; i < userBase.length; i++) {
+
+        isfound = true;
+
+        if (userBase[i].email == req.body.email) {
+            if (userBase[i].password == body.password) {
+                res.send(
+                    {
+                        "firstName": req.body.firstName,
+                        "lastName": req.body.lastName,
+                        "email": req.body.email,
+                        "message": "login successful"
+                    })
+            }
+
+            else {
+                res.status(401).send(
+                    { message: "your password is incorrect" }
+                    )
+            }
+        
+        
+        return;
+        }
+
+        if(!isfound){
+           res.end(
+            {
+                message : "user not found"
+            }
+           )
+           return;
+        }
+
+    }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })

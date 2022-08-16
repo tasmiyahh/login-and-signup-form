2e8af70250af180d77c1f080870aa1831da93c8b
tasmiyahh <tasmiyahhussainn99@gmail.com>
2022-08-16 12:27:39 +0500

    change

--- server.mjs
+++ server.mjs
@@ -1,104 +1,122 @@
-import express, { json } from 'express'
-import cors from 'cors';
-import { nanoid } from 'nanoid';
+import express from "express"
+import cors from "cors"
+import { nanoid } from 'nanoid'
+
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 const port = process.env.PORT || 3000;
 
-console.log(nanoid());
 
-let userBase = [];
+let userBase = []; // TODO: replace this with mongoDB
 
-//to create user
-app.post('/signup', (req, res) => {
+app.post("/signup", (req, res) => {
+
     let body = req.body;
 
-    if (!body.firstName || !body.lastName || !body.email || !body.password) {
-        res.status(400).send(`require field missing.please fill the require field eg:
-    {    
-        
-        "firstName" : "john" , 
-        "lastName" : "doe" ,
-        "email": "abc@gmail.com",
-        "password" : "12345"
-
-    }`);
-
+    if (!body.firstName
+        || !body.lastName
+        || !body.email
+        || !body.password
+    ) {
+        res.status(400).send(
+            `required fields missing, request example: 
+                {
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "email": "abc@abc.com",
+                    "password": "12345"
+                }`
+        );
         return;
     }
 
+    let isFound = false;
+
+    for (let i = 0; i < userBase.length; i++) {
+        if (userBase[i].email === body.email.toLowerCase()) {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound) { // this email already exist
+        res.status(400).send({
+            message: `email ${body.email} already exist.`
+        });
+        return;
+    }
+
+
     let newUser = {
-        "id": nanoid(),
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName,
-        "email": req.body.email,
-        "password": req.body.password
+        userId: nanoid(),
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email.toLowerCase(),
+        password: body.password
     }
 
     userBase.push(newUser);
-    res.status(201).send("user is created");
 
+    res.status(201).send({ message: "user is created" });
 });
 
-// post because login data is private or get ki body me openly jata h isliye post use
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
+
     let body = req.body;
 
     if (!body.email || !body.password) {
-        res.status(400).send(`require field missing.please fill the require field eg:
-    {    
-        
-        "firstName" : "john" , 
-        "lastName" : "doe" ,
-        "email": "abc@gmail.com",
-        "password" : "12345"
-
-    }`);
-
+        res.status(400).send(
+            `required fields missing, request example: 
+                {
+                    "email": "abc@abc.com",
+                    "password": "12345"
+                }`
+        );
         return;
     }
 
-    isfound = false ;
+    let isFound = false; // https://stackoverflow.com/a/17402180/4378475
 
     for (let i = 0; i < userBase.length; i++) {
+        if (userBase[i].email === body.email) {
 
-        isfound = true;
+            isFound = true;
+            if (userBase[i].password === body.password) { // correct password
 
-        if (userBase[i].email == req.body.email) {
-            if (userBase[i].password == body.password) {
-                res.send(
-                    {
-                        "firstName": req.body.firstName,
-                        "lastName": req.body.lastName,
-                        "email": req.body.email,
-                        "message": "login successful"
-                    })
+                res.status(200).send({
+                    firstName: userBase[i].firstName,
+                    lastName: userBase[i].lastName,
+                    email: userBase[i].email,
+                    message: "login successful",
+                    token: "some unique token"
+                })
+                return;
+
+            } else { // password incorrect
+
+                res.status(401).send({
+                    message: "incorrect password"
+                })
+                return;
             }
-
-            else {
-                res.status(401).send(
-                    { message: "your password is incorrect" }
-                    )
-            }
-        
-        
-        return;
         }
-
-        if(!isfound){
-           res.end(
-            {
-                message : "user not found"
-            }
-           )
-           return;
-        }
-
     }
-});
+
+    if (!isFound) {
+        res.status(404).send({
+            message: "user not found"
+        })
+        return;
+    }
+})
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
